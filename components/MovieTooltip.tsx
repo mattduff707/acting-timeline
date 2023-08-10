4045fc06bdfae5fee773f137f1ed9b612b38576b
Matthew Duffy <51209857+mattduff707@users.noreply.github.com>
2023-08-10 11:28:10 -0700

    Right side fade and tooltip layout

--- components/MovieTooltip.tsx
+++ components/MovieTooltip.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React from "react";
 
 const MovieTooltip = ({ movie }: { movie: any }) => {
   return (
-    <article className="flex flex-col w-[320px] h-[200px] p-4 bg-slate-500">
-      <header>
-        <h2 className="text-center">{movie.original_title}</h2>
+    <article className="flex flex-col w-full h-full py-2 px-4 bg-slate-700 overflow-auto text-slate-300">
+      <header className=" border-b-2 border-slate-600 pb-2">
+        <h2 className="text-center font-bold">{movie.original_title}</h2>
       </header>
-      <div className="flex">
-        <div className="grid place-items-center">
-          <img className="w-[100px] h-auto" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" />
+      <div className="flex gap-2 h-full">
+        <div className="grid place-items-center flex-[1]">
+          <img
+            className="w-[100px] h-auto"
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt=""
+          />
         </div>
-        <div>
-          <p>Character: {movie.character}</p>
-          <p>Release Date: {movie.release_date}</p>
-          <p>Rating: {movie.vote_average}</p>
+        <div className="flex-[2] flex flex-col justify-center">
+          <p>
+            <span className="font-bold">Character:</span> {movie.character}
+          </p>
+          <p>
+            <span className="font-bold">Release:</span> {movie.release_date}
+          </p>
+          <p>
+            <span className="font-bold">Rating:</span> {movie.vote_average}
+          </p>
         </div>
       </div>
     </article>
--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -1,6 +1,6 @@
-'use client';
-import { differenceInDays, getDaysInYear, getYear, isSameYear } from 'date-fns';
-import React from 'react';
+"use client";
+import { differenceInDays, getDaysInYear, getYear, isSameYear } from "date-fns";
+import React from "react";
 
 interface Point {
   x: number;
@@ -19,47 +19,50 @@ const Timeline = ({ x, y, points }: Props) => {
   const columns = `repeat(${x.length}, minmax(0, 1fr))`;
   const rows = `repeat(${y.length - 1}, minmax(0, 1fr))`;
 
-  const pointsByYear = points.reduce((acc: { [key: number]: { [key: number]: any[] } }, point) => {
-    const year = getYear(point.x);
+  const pointsByYear = points.reduce(
+    (acc: { [key: number]: { [key: number]: any[] } }, point) => {
+      const year = getYear(point.x);
 
-    const idxOfY = y.findIndex((yVal) => point.y <= yVal);
+      const idxOfY = y.findIndex((yVal) => point.y <= yVal);
 
-    if (idxOfY === -1) {
-      console.log('Value omitted: ' + point.y);
-      return acc;
-    }
+      if (idxOfY === -1) {
+        console.log("Value omitted: " + point.y);
+        return acc;
+      }
 
-    const category = idxOfY !== 0 ? y[idxOfY - 1] : y[idxOfY];
+      const category = idxOfY !== 0 ? y[idxOfY - 1] : y[idxOfY];
 
-    //if idx is 0 then do not subtract
+      //if idx is 0 then do not subtract
 
-    if (!acc[year]) {
-      return {
-        ...acc,
-        [year]: {
-          [category]: [point],
-        },
-      };
-    }
+      if (!acc[year]) {
+        return {
+          ...acc,
+          [year]: {
+            [category]: [point],
+          },
+        };
+      }
 
-    if (!acc[year][category]) {
+      if (!acc[year][category]) {
+        return {
+          ...acc,
+          [year]: {
+            ...acc[year],
+            [category]: [point],
+          },
+        };
+      }
+
       return {
         ...acc,
         [year]: {
           ...acc[year],
-          [category]: [point],
+          [category]: [...acc[year][category], point],
         },
       };
-    }
-
-    return {
-      ...acc,
-      [year]: {
-        ...acc[year],
-        [category]: [...acc[year][category], point],
-      },
-    };
-  }, {});
+    },
+    {}
+  );
 
   const boxes = y
     .slice()
@@ -82,7 +85,11 @@ const Timeline = ({ x, y, points }: Props) => {
               const dateDiff = differenceInDays(point.x, year);
               const leftPercentage = Math.round((dateDiff / daysInYear) * 100);
 
-              return { ...point, bottom: bottomPercentage, left: leftPercentage };
+              return {
+                ...point,
+                bottom: bottomPercentage,
+                left: leftPercentage,
+              };
             })
           : [];
 
@@ -92,17 +99,17 @@ const Timeline = ({ x, y, points }: Props) => {
         const isBorderRight = xIdx === x.length - 1;
 
         const borderLeft = isBorderLeft
-          ? { style: 'solid', width: '0px', color: 'rgb(100,116,139)' }
-          : { style: 'solid', width: '1px', color: 'rgb(71,85,105)' };
+          ? { style: "solid", width: "0px", color: "rgb(100,116,139)" }
+          : { style: "solid", width: "1px", color: "rgb(71,85,105)" };
         const borderTop = isBorderTop
-          ? { style: 'solid', width: '2px', color: 'rgb(71,85,105)' }
-          : { style: 'solid', width: '1px', color: 'rgb(71,85,105)' };
+          ? { style: "solid", width: "2px", color: "rgb(71,85,105)" }
+          : { style: "solid", width: "1px", color: "rgb(71,85,105)" };
         const borderBottom = isBorderBottom
-          ? { style: 'solid', width: '5px', color: 'rgb(100,116,139)' }
-          : { style: 'solid', width: '1px', color: 'rgb(71,85,105)' };
+          ? { style: "solid", width: "5px", color: "rgb(100,116,139)" }
+          : { style: "solid", width: "1px", color: "rgb(71,85,105)" };
         const borderRight = isBorderRight
-          ? { style: 'solid', width: '2px', color: 'rgb(71,85,105)' }
-          : { style: 'solid', width: '1px', color: 'rgb(71,85,105)' };
+          ? { style: "solid", width: "2px", color: "rgb(71,85,105)" }
+          : { style: "solid", width: "1px", color: "rgb(71,85,105)" };
 
         const isBottomHalf = yIdx > Math.floor(yArr.length / 2);
         const isNearEnd = xIdx > xArr.length - Math.floor(xArr.length / 4);
@@ -111,7 +118,7 @@ const Timeline = ({ x, y, points }: Props) => {
           <div
             key={`${xIdx}-${yIdx}`}
             style={{
-              borderColor: 'rgb(71,85,105)',
+              borderColor: "rgb(71,85,105)",
               //@ts-ignore
               borderLeftStyle: borderLeft.style,
               borderLeftWidth: borderLeft.width,
@@ -134,7 +141,9 @@ const Timeline = ({ x, y, points }: Props) => {
             {pointsWithPosition.map((point, idx, pointArr) => {
               const bottom = `calc(${point.bottom}% - 6px)`;
               const left = `calc(${point.left}% - 6px)`;
-              const transform = `translate(${isNearEnd ? '-100%' : '0%'}, ${isBottomHalf ? '-100%' : '0%'})`;
+              const transform = `translate(${isNearEnd ? "-100%" : "0%"}, ${
+                isBottomHalf ? "-100%" : "0%"
+              })`;
 
               return (
                 <div
@@ -146,9 +155,9 @@ const Timeline = ({ x, y, points }: Props) => {
                     key={`${point.x}-${point.y}`}
                     // Adjust top/left value if transformed
                     // style={{ transform: isBottomHalf ? 'translateY(-100%)' : 'translateY(0%)' }}
-                    className="group relative w-[12px] h-[12px] rounded-[50%] border-red-400 border-[6px] hover:w-[320px] hover:h-[200px] hover:z-[100] hover:rounded-[4px] z-[12] overflow-hidden hover:[transition:width_0.4s_0.2s_ease,height_0.4s_0.2s_ease,border-radius_0.2s_ease] [transition:width_0.4s_ease,height_0.4s_ease,border-radius_0.2s_0.4s_ease,z-index_0s_0.4s_ease]"
+                    className="group relative w-[12px] h-[12px] rounded-[50%] border-red-400 border-[6px] hover:w-[340px] hover:h-[240px] hover:z-[100] hover:rounded-[4px] z-[12] overflow-hidden hover:[transition:width_0.4s_0.2s_ease,height_0.4s_0.2s_ease,border-radius_0.2s_ease] [transition:width_0.4s_ease,height_0.4s_ease,border-radius_0.2s_0.4s_ease,z-index_0s_0.4s_ease]"
                   >
-                    <div className="z-[11] bg-slate-800 [&>*:first-child]:transition-opacity [&>*:first-child]:delay-0 [&>*:first-child]:duration-[0.4s] [&>*:first-child]:opacity-0 group-hover:[&>*:first-child]:opacity-100 group-hover:[&>*:first-child]:delay-[0.2s]">
+                    <div className="z-[11] bg-slate-800 w-full h-full [&>*:first-child]:transition-opacity [&>*:first-child]:delay-0 [&>*:first-child]:duration-[0.4s] [&>*:first-child]:opacity-0 group-hover:[&>*:first-child]:opacity-100 group-hover:[&>*:first-child]:delay-[0.5s]">
                       {point.hoverComponent}
                     </div>
                   </div>
@@ -162,7 +171,7 @@ const Timeline = ({ x, y, points }: Props) => {
     });
 
   return (
-    <div className="h-full grid w-[fit-content] grid-cols-[120px_1fr] grid-rows-[1fr_120px] pt-8 pr-20 text-slate-300 isolate">
+    <div className="h-full grid w-[fit-content] grid-cols-[120px_1fr_120px] grid-rows-[1fr_120px] pt-8 pr-20 text-slate-300 isolate">
       <div className="sticky left-0 border-r-[5px] bg-slate-800 border-slate-500 pr-2 z-20">
         <div className="relative h-full">
           {y
@@ -173,7 +182,7 @@ const Timeline = ({ x, y, points }: Props) => {
               return (
                 <div
                   key={yVal}
-                  style={{ top, right: '8px', transform: 'translateY(-50%)' }}
+                  style={{ top, right: "8px", transform: "translateY(-50%)" }}
                   className="absolute text-slate-300 text-xl"
                 >
                   {yVal}
@@ -182,20 +191,29 @@ const Timeline = ({ x, y, points }: Props) => {
             })}
         </div>
       </div>
-      <div style={{ width, gridTemplateColumns: columns, gridTemplateRows: rows }} className={`h-full grid`}>
+      <div
+        style={{ width, gridTemplateColumns: columns, gridTemplateRows: rows }}
+        className={`h-full grid`}
+      >
         {boxes.flat().map((box) => box)}
       </div>
+      <div className="sticky right-0 bg-gradient-to-r from-transparent to-slate-800 z-[30]"></div>
       <div className="bg-gradient-to-r from-slate-800 to-transparent sticky left-0 z-[13] border-t-8 border-slate-800" />
       <div className="relative pt-2">
         {x.map((xVal, idx, arr) => {
           const left = `${idx * 140}px`;
           return (
-            <div key={xVal} style={{ left }} className="absolute translate-x-[-50%] text-xl">
+            <div
+              key={xVal}
+              style={{ left }}
+              className="absolute translate-x-[-50%] text-xl"
+            >
               {xVal}
             </div>
           );
         })}
       </div>
+      <div className="sticky right-0 bg-gradient-to-r from-transparent to-slate-800 z-[30]"></div>
     </div>
   );
 };
